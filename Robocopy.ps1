a62c7fa7bfeaed7bf57798345af5db17a355cabe
Robert Place <36657717+robertPlace@users.noreply.github.com>
2021-04-30 10:32:11 -0700

    Update Robocopy.ps1

--- Robocopy.ps1
+++ Robocopy.ps1
@@ -1,4 +1,4 @@
 $SourcePath = '\\location\to\your\file'
-$DestinationPath = 'D:\'
+$DestinationPath = 'D:\where\you\want\it\to\go'
 $Logfile = 'c:\temp\robocopy.txt'
 Robocopy $SourcePath $DestinationPath /e /z /dcopy:dat /copyall /r:5 /w:5 /log:$logfile /tee /np 
